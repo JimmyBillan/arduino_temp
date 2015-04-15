341cff75043e8183069ab52db9dee278aed5ae62
moukrim <moukrim_yassine@hotmail.fr>
2015-04-15 15:48:34 +0200

    Update postTemp.php

--- postTemp.php
+++ postTemp.php
@@ -10,12 +10,4 @@ if(!empty($_POST["t"]) && isset($_POST["s"]) && isset($_POST["b"])){
   $conn->close();
 }
 
-  $t = rand(21.00 ,25.00);
-  $conn = new mysqli("localhost", "root", "root", "projet_temp");
-  if ($conn->connect_error) {die("Connection failed: " . $conn->connect_error);} 
-   $sql = "INSERT INTO temperatures (nom_salle, temperature_salle, datePost, batterie)VALUES (  'salle224', '".$t."', NOW(), '".rand(70.00 ,80.00)."')";
-
-  if ($conn->query($sql) === TRUE){echo "New record created successfully";}else {echo "Error: " . $sql . "<br>" . $conn->error;}
-
-
-$conn->close();
+  
